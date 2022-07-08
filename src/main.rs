52ea97268596b00ebd6faedcc3073920c1746be3
Bastien VERSINI <bastien.versini@mailbox.org>
2022-07-08 22:15:06 +0200

    feat: argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
+use clap::{Parser, Subcommand};
 use kanji_practice_sheet::{arg_parsing::kanji_to_filename, pdf_creation::kanji_to_png};
 
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Args {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Start the webserver
+    Server {},
+    /// Use the generator directly
+    #[clap(arg_required_else_help = true)]
+    Cli {
+        //TODO interactive mode?
+        kanjis: String,
+    },
+}
 fn main() {
-    let now = std::time::Instant::now();
-    kanji_to_png(&kanji_to_filename('家'));
-    println!("{:?}", now.elapsed());
+    let args = Args::parse();
+    match args.command {
+        Commands::Server {} => {}
+        Commands::Cli { kanjis } => {
+            // if let Some(k) = kanjis.chars().nth(0) {
+            let now = std::time::Instant::now();
+            string_to_png(&kanjis);
+            // kanji_to_png(&kanji_to_filename(k));
+            println!("{:?}", now.elapsed());
+            // }
+        }
+    };
 }
